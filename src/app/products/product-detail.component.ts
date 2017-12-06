d823e5481f8ab16dce77c09a6d4173cbe53cf34a
Bruno Luis Barrios Vargas <Bruno.Barrios@jalasoft.com>
2017-12-06 19:23:08 -0400

    fix product detail

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -7,16 +7,21 @@ import { ProductService } from "./product.service";
 @Component({
     templateUrl: "./product-detail.component.html"
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
      pageTitle: string = "Product Detail";
      product: IProduct;
      errorMessage: string;
 
-     constructor(private _route: ActivatedRoute, private _router: Router, private _productService: ProductService) {}
+     constructor(private _route: ActivatedRoute,
+         private _router: Router,
+         private _productService: ProductService) {}
 
      ngOnInit(): void {
-         let id = +this._route.snapshot.params['id'];
-         this.pageTitle +=  `:  ${id}`;
+        const param = this._route.snapshot.paramMap.get('id');
+        if (param) {
+          const id = +param;
+          this.getProduct(id);
+        }
      }
 
      getProduct(id: number) {
